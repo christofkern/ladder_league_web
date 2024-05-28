9cc00cee7882f08ebaa328fe59a7b133e3e78040
christofkern <79835127+christofkern@users.noreply.github.com>
2024-05-28 07:27:54 +0200

    fix: sob when multiple tcs versions on therun

--- therun_user_info.py
+++ therun_user_info.py
@@ -11,19 +11,20 @@ def isEmpty(dictionary):
 
 def get_runner_sob(runner):
     response = requests.get(f"https://therun.gg/api/users/{runner}/")
-    # Check the status of the API
+    # Check the status of the API   
     #print(response.status_code)
-
-
     if response.status_code == 500:
         return "--:--:--"
     data = response.json()
+    sw_sob = float('inf')
+    #print(data)
     for gamedata in data:
         game = gamedata["game"]
-        category = gamedata["run"]       
-        if ("variables" not in gamedata):
-            return "--:--:--"
-        solo = gamedata["variables"]
+        category = gamedata["run"]    
+        if ("variables" in gamedata): 
+            solo = gamedata["variables"]
+        else:
+            solo = {}
         sob = gamedata["sumOfBests"]
         #print(f"SOB of {runner} is {sob}")
         if ("Solo or Co-op?" in solo):
@@ -33,9 +34,11 @@ def get_runner_sob(runner):
         if ("LEGO Star Wars: The Complete Saga (PC/Console)" in game and "Any%" in category and ("Solo" in solo or solo == {})):  
             #print(gamedata)
             if (gamedata["gameTimeData"] is not None):  
-                #use loadless if available
+                #use loadless if available		 
                 sob = gamedata["gameTimeData"]["sumOfBests"]
-            return format_milliseconds(sob)
+                if (float(sob) < sw_sob):
+                    sw_sob = sob    
+    return format_milliseconds(sw_sob)
 
 
 def get_runner_bpt(race_id, rungg):
